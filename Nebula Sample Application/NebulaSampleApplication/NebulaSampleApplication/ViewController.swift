d70a47763bf003d02ba30c0034d6b19cfeb1d893
Alois Barreras <alois@vicino.io>
2016-10-06 01:05:18 -0400

    adding core data

---
+++ Nebula Sample Application/NebulaSampleApplication/NebulaSampleApplication/ViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  NebulaSampleApplication
+//
+//  Created by Craig Barreras on 10/6/16.
+//  Copyright © 2016 Vicino. All rights reserved.
+//
+
+import UIKit
+import CoreData
+
+private let ReuseIdentifier = "Cell"
+
+class ViewController: UIViewController {
+    
+//    fileprivate var frc: NSFetchedResultsController!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+}
+
+//extension ViewController: UITableViewDataSource {
+//    func numberOfSections(in tableView: UITableView) -> Int {
+//        
+//    }
+//    
+//    
+//}
